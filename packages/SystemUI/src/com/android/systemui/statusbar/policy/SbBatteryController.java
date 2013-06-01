e378d9e263bee48722e55f9050e186ee2a4d5f9c
Alex <mazdarider23@gmail.com>
2013-06-01 18:21:01 -0500

    Moar Batteries :)
    Change-Id: I45149c7501cdbd07749f68fd5b7250fcad0d6b70

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/SbBatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/SbBatteryController.java
@@ -74,11 +74,12 @@ public class SbBatteryController extends LinearLayout {
     public static final int STYLE_ICON_TEXT = 2;
     public static final int STYLE_ICON_CENTERED_TEXT = 3;
     public static final int STYLE_ICON_CIRCLE = 4;
-    public  static final int BATTERY_STYLE_CIRCLE         = 5;
-    public  static final int BATTERY_STYLE_CIRCLE_PERCENT = 6;
-    public  static final int BATTERY_STYLE_DOTTED_CIRCLE_PERCENT = 7;
-	public static final int STYLE_ICON_AXIOM = 8;
-    public static final int STYLE_HIDE = 9;
+    public static final int BATTERY_STYLE_CIRCLE         = 5;
+    public static final int BATTERY_STYLE_CIRCLE_PERCENT = 6;
+    public static final int BATTERY_STYLE_DOTTED_CIRCLE_PERCENT = 7;
+    public static final int STYLE_ICON_AXIOM = 8;
+    public static final int STYLE_ICON_CM = 9;
+    public static final int STYLE_HIDE = 10;
 
     public SbBatteryController(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -151,6 +152,10 @@ public class SbBatteryController extends LinearLayout {
             case STYLE_ICON_AXIOM:
                  icon = plugged ? R.drawable.stat_sys_battery_charge_axiom
                  : R.drawable.stat_sys_battery_axiom;
+                 break;
+            case STYLE_ICON_CM:
+                 icon = plugged ? R.drawable.stat_sys_battery_charge_min
+                 : R.drawable.stat_sys_battery_min;
                  break;
             default:
                  icon = plugged ? R.drawable.stat_sys_battery_charge
@@ -296,6 +301,12 @@ public class SbBatteryController extends LinearLayout {
                 setVisibility(View.VISIBLE);
                 break;
             case STYLE_ICON_AXIOM:
+                mBatteryText.setVisibility(View.GONE);
+                mBatteryCenterText.setVisibility(View.GONE);
+                mBatteryIcon.setVisibility(View.VISIBLE);
+                setVisibility(View.VISIBLE);
+                break;
+            case STYLE_ICON_CM:
                 mBatteryText.setVisibility(View.GONE);
                 mBatteryCenterText.setVisibility(View.GONE);
                 mBatteryIcon.setVisibility(View.VISIBLE);
