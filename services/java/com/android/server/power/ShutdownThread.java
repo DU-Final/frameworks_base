73cff61c8b516292205ac5b3440c2a30423d5490
Alex <mazdarider23@gmail.com>
2013-10-01 02:27:50 -0500

    Added custom BusyDialog from ChaOS
    Credit and Thanks to 0xD34D / n3ocort3x
    
    Change-Id: I33dcd1b2ca940f24bdf2fbced799b846e5d58e13

--- services/java/com/android/server/power/ShutdownThread.java
+++ services/java/com/android/server/power/ShutdownThread.java
@@ -14,11 +14,12 @@
  * limitations under the License.
  */
 
- 
+
 package com.android.server.power;
 
 import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
+import android.app.PacBusyDialog;
 import android.app.Dialog;
 import android.app.IActivityManager;
 import android.app.ProgressDialog;
@@ -267,7 +268,7 @@ public final class ShutdownThread extends Thread {
 
         // throw up an indeterminate system dialog to indicate radio is
         // shutting down.
-        ProgressDialog pd = new ProgressDialog(context);
+        PacBusyDialog pd = new PacBusyDialog(context, android.R.style.Theme_Translucent_NoTitleBar);
         if (mReboot) {
             if (mRebootHot) {
                 pd.setTitle(context.getText(com.android.internal.R.string.hot_reboot_title));
@@ -280,9 +281,13 @@ public final class ShutdownThread extends Thread {
             pd.setTitle(context.getText(com.android.internal.R.string.power_off));
             pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
         }
-        pd.setIndeterminate(true);
-        pd.setCancelable(false);
+        //pd.setIndeterminate(true);
         pd.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
+  pd.getWindow().addFlags(
+                 WindowManager.LayoutParams.FLAG_DIM_BEHIND
+                 | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        pd.getWindow().setDimAmount(1);
+        pd.setCancelable(false);
 
         pd.show();
 
