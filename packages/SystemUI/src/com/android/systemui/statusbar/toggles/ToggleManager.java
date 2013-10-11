nazunamoe/android_frameworks_base

Toggle: Volume Toggle
Based on CM's - https://github.com/CyanogenMod/android_frameworks_base/commit/d2b7a0e0b0c5a6fb350214858a9f2468fd3b9e60

Signed-off-by: Daniel <daxx4182@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -57,6 +57,8 @@ public class ToggleManager {
 
     private static final String TOGGLE_PIPE = "|";
 
+    public static final String XYLON_TOGGLE = "XYLON";
+
     public static final String USER_TOGGLE = "USER";
     public static final String BRIGHTNESS_TOGGLE = "BRIGHTNESS";
     public static final String SETTINGS_TOGGLE = "SETTINGS";
@@ -83,6 +85,7 @@ public class ToggleManager {
     public static final String THREEG_TOGGLE = "3G";
     public static final String FAV_CONTACT_TOGGLE = "FAVCONTACT";
     public static final String SOUND_STATE_TOGGLE = "SOUNDSTATE";
+    public static final String VOLUME_TOGGLE = "VOLUME";
     public static final String NAVBAR_HIDE_TOGGLE = "NAVBARHIDE";
     public static final String QUICKRECORD_TOGGLE = "QUICKRECORD";
     public static final String SLEEP_TOGGLE = "SLEEP";
@@ -91,7 +94,6 @@ public class ToggleManager {
     public static final String QUIETHOURS_TOGGLE = "QUIETHOURS";
     public static final String PIE_TOGGLE = "PIE";
     public static final String SCREENSHOT_TOGGLE = "SCREENSHOT";
-    public static final String XYLON_TOGGLE = "XYLON";
 
     private int mStyle;
     private boolean mShowRebootOnLock = true;
@@ -116,6 +118,7 @@ public class ToggleManager {
             toggleMap.put(USER_TOGGLE, UserToggle.class);
             toggleMap.put(BRIGHTNESS_TOGGLE, BrightnessToggle.class);
             toggleMap.put(SETTINGS_TOGGLE, SettingsToggle.class);
+            toggleMap.put(XYLON_TOGGLE, XylonToggle.class);
             toggleMap.put(WIFI_TOGGLE, WifiToggle.class);
             toggleMap.put(SIGNAL_TOGGLE, SignalToggle.class);
             toggleMap.put(ROTATE_TOGGLE, RotateToggle.class);
@@ -138,6 +141,7 @@ public class ToggleManager {
             toggleMap.put(THREEG_TOGGLE, ThreeGToggle.class);
             toggleMap.put(FAV_CONTACT_TOGGLE, FavoriteUserToggle.class);
             toggleMap.put(SOUND_STATE_TOGGLE, SoundStateToggle.class);
+            toggleMap.put(VOLUME_TOGGLE, VolumeToggle.class);
             toggleMap.put(NAVBAR_HIDE_TOGGLE, NavbarHideToggle.class);
             toggleMap.put(QUICKRECORD_TOGGLE, QuickRecordToggle.class);
             toggleMap.put(SLEEP_TOGGLE, SleepToggle.class);
@@ -146,7 +150,6 @@ public class ToggleManager {
             toggleMap.put(QUIETHOURS_TOGGLE, QuietHoursToggle.class);
             toggleMap.put(PIE_TOGGLE, PieToggle.class);
             toggleMap.put(SCREENSHOT_TOGGLE, ScreenshotToggle.class);
-            toggleMap.put(XYLON_TOGGLE, XylonToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;

DU-Final/frameworks_base

Change-Id: I249285ba0be3bd4b147043c2865338f97b95e9b2

packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -89,6 +89,7 @@ public class ToggleManager {
     public static final String LTE_TOGGLE = "LTE";
     public static final String FAV_CONTACT_TOGGLE = "FAVCONTACT";
     public static final String SOUND_STATE_TOGGLE = "SOUNDSTATE";
+    public static final String VOLUME_TOGGLE = "VOLUME";
     public static final String NAVBAR_HIDE_TOGGLE = "NAVBARHIDE";
     public static final String QUICKRECORD_TOGGLE = "QUICKRECORD";
     public static final String QUIETHOURS_TOGGLE = "QUIETHOURS";
@@ -161,6 +162,8 @@ public class ToggleManager {
                 toggleMap.put(LTE_TOGGLE, LteToggle.class);
             }
             toggleMap.put(FAV_CONTACT_TOGGLE, FavoriteUserToggle.class);
+            toggleMap.put(SOUND_STATE_TOGGLE, SoundStateToggle.class);
+            toggleMap.put(VOLUME_TOGGLE, VolumeToggle.class);
             toggleMap.put(NAVBAR_HIDE_TOGGLE, NavbarHideToggle.class);
             toggleMap.put(QUICKRECORD_TOGGLE, QuickRecordToggle.class);
             toggleMap.put(QUIETHOURS_TOGGLE, QuietHoursToggle.class);