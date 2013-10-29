03fa87b527af53cc3b66567db864235e9041f301
Alex <mazdarider23@gmail.com>
2013-10-28 21:03:53 -0500

    Squashed BT commits from CM
    Change-Id: I6c4caea5031faa5f3393bd073db8be38140f7053

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -151,6 +151,7 @@ class ServerThread extends Thread {
         BluetoothManagerService bluetooth = null;
         DockObserver dock = null;
         RotationSwitchObserver rotateSwitch = null;
+        RegulatoryObserver regulatory = null;
         UsbService usb = null;
         SerialService serial = null;
         TwilightService twilight = null;
@@ -486,6 +487,14 @@ class ServerThread extends Thread {
                 ServiceManager.addService(Context.NETWORK_POLICY_SERVICE, networkPolicy);
             } catch (Throwable e) {
                 reportWtf("starting NetworkPolicy Service", e);
+            }
+
+            try {
+                Slog.i(TAG, "Regulatory Observer");
+                // Listen for country code changes
+                regulatory = new RegulatoryObserver(context);
+            } catch (Throwable e) {
+                reportWtf("starting RegulatoryObserver", e);
             }
 
            try {
